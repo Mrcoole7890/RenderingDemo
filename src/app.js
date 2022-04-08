f36b91be76e3220787e3b07552f1293354c6f854
Cole A. Kendall <mrcoole7890@outlook.com>
2022-04-08 16:25:41 -0400

    make public folder accessible to front end

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.send("this works");
+    res.sendFile("public/index.html", {root: __dirname});
 });
 
 app.listen(8081, () => {
