cd7a7abfa30b1d4d6f1c9270f6f218def18c252b
Cole A. Kendall <mrcoole7890@outlook.com>
2022-04-08 16:28:09 -0400

    add buttons for handling the sprite upload process

--- src/public/index.js
+++ src/public/index.js
@@ -5,9 +5,17 @@ fetch("Resources//RenderingSpecs//testSpecs.json")
 function init(specs) {
 
     let renderer = specs.renderer;
+    let gui = specs.gui;
 
     let app = new PIXI.Application({ width: renderer.width, height: renderer.height });
+    let newSpriteButton = document.createElement(gui.addSpriteButton.DOMElement.tag);
+    newSpriteButton.setAttribute("id", gui.addSpriteButton.DOMElement.id);
+    newSpriteButton.setAttribute("type", gui.addSpriteButton.DOMElement.type);
+    newSpriteButton.setAttribute("value", gui.addSpriteButton.DOMElement.value);
+    
     document.getElementById(renderer.idOfLocation).appendChild(app.view);
+    document.getElementById(renderer.idOfLocation).appendChild(document.createElement("br"));
+    document.getElementById(gui.addSpriteButton.idOfLocation).appendChild(newSpriteButton);
 
     let textureArray = [];
 
@@ -22,6 +30,8 @@ function init(specs) {
     animatedSprite.play();
     app.stage.addChild(animatedSprite);
 
+    newSpriteButton.addEventListener(gui.addSpriteButton.DOMElement.redirectEvent, (e) => {window.location.href = gui.addSpriteButton.redirect;});
+
 }
 
 function loadTextures(textureArr, folderPath, fileExtention, numOfFrames){
@@ -31,6 +41,8 @@ function loadTextures(textureArr, folderPath, fileExtention, numOfFrames){
         textureArr.push(texture);
     };
 }
+
+
 
 
 
